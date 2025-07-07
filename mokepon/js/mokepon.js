cde9502f6e41f76d3e99cd41f879ce7ad76be69e
Karoly Torres Pacheco <karolytorrespacheco@gmail.com>
2025-07-07 13:29:35 -0500

    función iniciar el juego

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -1,6 +1,10 @@
-function seleccionarMascotaJugador(){
-    alert("Seleccionaste tu mascota")
+function iniciarJuego(){
+    let botonMascotaJugador = document.getElementById('boton-mascota') 
+    botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
 }
 
-let botonMascotaJugador = document.getElementById('boton-mascota') 
-botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
+function seleccionarMascotaJugador(){
+    alert("Seleccionaste tu mascota")
+} 
+
+window.addEventListener('load', iniciarJuego)
