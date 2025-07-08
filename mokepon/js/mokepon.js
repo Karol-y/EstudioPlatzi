a478ff262916b31fc3c3e7edbecb8d693301e19d
Karoly Torres Pacheco <karolytorrespacheco@gmail.com>
2025-07-07 20:30:47 -0500

    selección de mascotas

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -4,7 +4,19 @@ function iniciarJuego(){
 }
 
 function seleccionarMascotaJugador(){
-    alert("Seleccionaste tu mascota")
+    let inputHipodoge = document.getElementById('hipodoge')
+    let inputCapipepo = document.getElementById('capipepo')
+    let inputRatigueya = document.getElementById('ratigueya')
+
+    if(inputHipodoge.checked){
+        alert("Seleccionaste a Hipodoge")
+    } else if(inputCapipepo.checked){
+        alert("Seleccionaste a Capipepo")
+    } else if(inputRatigueya.checked){
+        alert("Seleccionaste a Ratigueya")
+    } else {
+        alert("Debes seleccionar una Mascota")
+    }
 } 
 
 window.addEventListener('load', iniciarJuego)
